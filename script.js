1f03d8815a6bcf2a2e3724f53d01c80b257690f0
Ganesh Pathan <118661019+GaneshPathan1015@users.noreply.github.com>
2023-12-29 15:23:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	const form1 = document.getElementById("form1");
+	let str = form1.elements.fname.value+" " + form1.elements.lname.value
+	alert(str);
 
 }
